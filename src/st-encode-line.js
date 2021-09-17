8cfe8c7999d361258a310b29708c5f933403be6a
Anton Ustyuzhanin <au@hellofresh.com>
2021-09-17 12:30:36 +0200

    feat: (ST) Encode line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  if (str.length == 0) {
+    return '';
+  }
+
+  let res = [];
+  let last = str[0];
+  let counter = 0;
+
+  for (let c of str) {
+    if (c == last) {
+      counter++;
+      continue;
+    }
+    res.push([counter, last]);
+    last = c;
+    counter = 1;
+  }
+  res.push([counter, last]);
+
+  return res.flat().filter(a => a != 1).join('');
 }
