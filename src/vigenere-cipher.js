2e0acaca77da98d7b78ea3f0c8f18495e9aa65ab
Anton Ustyuzhanin <au@hellofresh.com>
2021-09-17 11:09:20 +0200

    feat: Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,29 +3,110 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
-export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+function isEligible(char) {
+  return char.charCodeAt(0) >= 'A'.charCodeAt(0) && char.charCodeAt(0) <= 'Z'.charCodeAt(0);
+}
+
+function getKey(str, key) {
+  return str.split('').map((_, i) => key[i % key.length]).join('');
+}
+
+function compress(str) {
+  return str.split('')
+    .filter(a => isEligible(a))
+    .join('');
+}
+
+function decompress(str, chars) {
+  chars = chars.split('');
+  for (let i = 0; i < str.length; i++) {
+    if (isEligible(str[i])) {
+      continue;
+    }
+    chars.splice(i, 0, str[i]);
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return chars.join('');
+}
+
+function getCharNumber(char) {
+  return char.charCodeAt(0) - 'A'.charCodeAt(0);
+}
+
+function encryptChar(char, keyChar) {
+  return String.fromCharCode(((getCharNumber(char) + getCharNumber(keyChar)) % 26) + 'A'.charCodeAt(0))
+}
+
+function decryptChar(char, keyChar) {
+  return String.fromCharCode(((getCharNumber(char) - getCharNumber(keyChar) + 26) % 26) + 'A'.charCodeAt(0))
+}
+
+export default class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this._direct = direct;
+  }
+
+  encrypt(str, key) {
+    if (arguments.length < 2 || typeof str === 'undefined' || typeof key === 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+
+    let chars = compress(str);
+    let cryptoKey = getKey(chars, key);
+
+    chars = chars.split('')
+      .map((a, i) => encryptChar(a, cryptoKey[i]))
+      .join('');
+
+    chars = decompress(str, chars);
+
+    if (!this._direct) {
+      chars = chars.split('').reverse().join('');
+    }
+
+    return chars;
+  }
+
+  decrypt(str, key) {
+    if (arguments.length < 2 || typeof str === 'undefined' || typeof key === 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+
+    let chars = compress(str);
+    let cryptoKey = getKey(chars, key);
+
+    chars = chars.split('')
+      .map((a, i) => decryptChar(a, cryptoKey[i]))
+      .join('');
+
+    chars = decompress(str, chars);
+
+    if (!this._direct) {
+      chars = chars.split('').reverse().join('');
+    }
+
+    return chars;
   }
 }
