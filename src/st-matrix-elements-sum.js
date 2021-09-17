6b62165619bb6d2e2dcab4b887267beb4dfd3fa3
Anton Ustyuzhanin <au@hellofresh.com>
2021-09-17 16:12:07 +0200

    feat: (ST) Matrix elements sum

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -16,7 +16,17 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getMatrixElementsSum(matrix) {
+  let candidates = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i > 0 && matrix[i-1][j] === 0) {
+        continue;
+      }
+      candidates.push(matrix[i][j]);
+    }
+  }
+
+  return candidates.reduce((s, a) => s + a, 0);
 }
